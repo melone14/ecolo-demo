b287627ca46898f9a92a495fb033635ff23a714e
Wojciech Melone <w.melone97@gmail.com>
2020-08-25 22:09:32 +0100

    Add more content

---
+++ app.js
@@ -0,0 +1,42 @@
+const getDate = () => {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  today = yyyy + "-" + mm + "-" + dd;
+  document.getElementById("date").value = today;
+};
+
+window.onload = function () {
+  getDate();
+};
+
+const counters = document.querySelectorAll(".counter");
+const speed = 2000;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+
+  updateCount();
+});
